7a6d8e607be2ec7996da6080f9b1edbfff766d51
SnowLLL <73144861+SnowLLL@users.noreply.github.com>
2021-03-16 20:01:26 -0700

    Add files via upload

---
+++ src/Slider.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Slider from '@material-ui/core/Slider';
+import Input from '@material-ui/core/Input';
+
+const useStyles = makeStyles({
+  root: {
+    width: 150,
+  },
+  input: {
+    width: 45,
+  },
+});
+
+const InputSlider = (props) => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0.5);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+    props.sliderValueCallback (newValue);
+    console.log('Send slider volume is ' + newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+    props.sliderValueCallback (Number(event.target.value));
+    console.log('Send input volume is ' + Number(event.target.value));
+  };
+
+  return (
+    <div className={classes.root}>
+      <div>
+            <Grid item>
+                <label className='me-2'>Volume</label>
+                <Input
+                    className={classes.input}
+                    value={value}
+                    margin="dense"
+                    onChange={handleInputChange}
+                    inputProps={{
+                    step: 0.1,
+                    min: 0,
+                    max: 1,
+                    type: 'number',
+                    'aria-labelledby': 'input-slider',
+                    }}
+                />
+            </Grid>
+      </div>
+      <Grid container spacing={3} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            value ={value}
+            min = {0}
+            max = {1}
+            step = {0.1}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default InputSlider
---
+++ src/constants.js
@@ -0,0 +1,78 @@
+
+
+export const audios = [
+    {
+        name: 'Q',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    },
+    {
+        name: 'W',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+        name: 'E',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    {
+        name: 'A',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    {
+        name: 'S',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+        name: 'D',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+        name: 'Z',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+        name: 'X',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+        name: 'C',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    }
+];
+export const audiosBank = [
+    {
+        name: 'QB',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    },
+    {
+        name: 'WB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    },
+    {
+        name: 'EB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    },
+    {
+        name: 'AB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    },
+    {
+        name: 'SB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+    },
+    {
+        name: 'DB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+    },
+    {
+        name: 'ZB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+    },
+    {
+        name: 'XB',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+    },
+    {
+        name: 'CB',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+    }
+];
